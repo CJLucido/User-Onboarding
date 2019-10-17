583caddb8eb0c678151440cfc7356419787f9911
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-17 15:30:30 -0700

    fix test

--- user-onboarding/cypress/integration/AppBasics.spec.js
+++ user-onboarding/cypress/integration/AppBasics.spec.js
@@ -1,3 +1,9 @@
 describe("It should load the App page at the baseUrl", () => {
     beforeEach(()=>{cy.visit("/")})
+    it("ensures the title loads as an h1", () => {
+        cy.get('h1').should('be.visible')
+    })
+    it.only("ensures that a form is displayed", () => {
+        cy.get("FormikProjectForm").should("be.visible")
+    })
 })
